bbfb9bba49bbce77706026ea4c53867f63c7fe91
CarolynnFleming <cfcatpink@gmail.com>
2022-04-25 16:19:13 -0700

    added imports, and git test started

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,11 @@
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import Header from './components/layout/Header'
 
-test('Should render the header', () => {
-
-})
+test('Should render the header', async () => {
+render(
+    <MemoryRouter>
+     <Header />
+    </MemoryRouter>
+)
+});
